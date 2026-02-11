cc1797e6325c947d7a6346550c0e57dac7fedf18
Slivka <max04032007@gmail.com>
2026-02-11 15:30:29 +0200

    Update NoteService.java

--- src/main/java/ua/opnu/project/service/NoteService.java
+++ src/main/java/ua/opnu/project/service/NoteService.java
@@ -13,33 +13,30 @@ public class NoteService {
 
     private final NoteRepository noteRepository;
 
-    // Створення нотатки
+
     public Note create(Note note) {
         return noteRepository.save(note);
     }
 
-    // Отримання всіх нотаток
+
     public List<Note> getAll() {
         return noteRepository.findAll();
     }
 
-    // Пошук за ID (повертаємо сутність або кидаємо помилку)
+
     public Note getById(Long id) {
         return noteRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Note not found with id: " + id));
     }
 
-    // Оновлення нотатки
     public Note update(Long id, Note updatedNoteData) {
-        Note existingNote = getById(id); // Використовуємо наш метод пошуку
-
+        Note existingNote = getById(id); 
         existingNote.setTitle(updatedNoteData.getTitle());
         existingNote.setContent(updatedNoteData.getContent());
 
         return noteRepository.save(existingNote);
     }
 
-    // Видалення нотатки
     public void delete(Long id) {
         if (!noteRepository.existsById(id)) {
             throw new RuntimeException("Note not found with id: " + id);
